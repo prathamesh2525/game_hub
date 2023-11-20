25fbe54fc1230a4676a61721c4cea40bcaa6d639
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2023-11-20 13:39:18 +0530

    displayed platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react"
 import apiClient from "../services/api-client"
 import { CanceledError } from "axios"
 
+export interface Platform {
+    id: number
+    name: string
+    slug: string
+}
+
 export interface Game {
     id: number
     name: string
     background_image: string
+    parent_platforms: { platform: Platform }[]
 }
 
 interface GameProps {
@@ -14,6 +21,7 @@ interface GameProps {
 }
 
 const useGames = () => {
+
     const [games, setGames] = useState<Game[]>([])
     const [error, setError] = useState('')
 
