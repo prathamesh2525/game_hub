983ed51e4ca52cf773a4951881d9b051234a0e1a
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2023-11-26 23:32:58 +0530

    created a genric data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer"
 
 const GameGrid = () => {
 
-    const { error, games, isLoading } = useGames()
+    const { error,data: games, isLoading } = useGames()
 
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     return (
